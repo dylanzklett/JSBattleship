7d393f3cb130cc0536197cc3b9bdd319c29b1076
Dylan Klett <dylanzklett@gmail.com>
2019-06-02 15:20:57 -0400

    initial commit

---
+++ battleship.js
@@ -0,0 +1,302 @@
+//Add event listeners to squares for calling functions
+	//Square Row 1 Event Listeners:
+	var g1s1 = document.getElementById("g1s1");
+	g1s1.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g1s1.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g1s2 = document.getElementById("g1s2");
+	g1s2.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g1s2.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g1s3 = document.getElementById("g1s3");
+	g1s3.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g1s3.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g1s4 = document.getElementById("g1s4");
+	g1s4.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g1s4.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g1s5 = document.getElementById("g1s5");
+	g1s5.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g1s5.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g1s6 = document.getElementById("g1s6");
+	g1s6.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g1s6.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	//Square Row 2 Event Listeners:
+
+	var g2s1 = document.getElementById("g2s1");
+	g2s1.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g2s1.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g2s2 = document.getElementById("g2s2");
+	g2s2.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g2s2.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g2s3 = document.getElementById("g2s3");
+	g2s3.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g2s3.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g2s4 = document.getElementById("g2s4");
+	g2s4.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g2s4.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g2s5 = document.getElementById("g2s5");
+	g2s5.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g2s5.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g2s6 = document.getElementById("g2s6");
+	g2s6.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g2s6.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	//Square Row 3 Event Listeners:
+
+	var g3s1 = document.getElementById("g3s1");
+	g3s1.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g3s1.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g3s2 = document.getElementById("g3s2");
+	g3s2.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g3s2.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g3s3 = document.getElementById("g3s3");
+	g3s3.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g3s3.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g3s4 = document.getElementById("g3s4");
+	g3s4.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g3s4.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g3s5 = document.getElementById("g3s5");
+	g3s5.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g3s5.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g3s6 = document.getElementById("g3s6");
+	g3s6.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g3s6.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	//Square Row 4 Event Listeners:
+
+	var g4s1 = document.getElementById("g4s1");
+	g4s1.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g4s1.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g4s2 = document.getElementById("g4s2");
+	g4s2.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g4s2.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g4s3 = document.getElementById("g4s3");
+	g4s3.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g4s3.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g4s4 = document.getElementById("g4s4");
+	g4s4.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g4s4.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g4s5 = document.getElementById("g4s5");
+	g4s5.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g4s5.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g4s6 = document.getElementById("g4s6");
+	g4s6.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g4s6.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	//Square Row 5 Event Listeners:
+
+	var g5s1 = document.getElementById("g5s1");
+	g5s1.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g5s1.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g5s2 = document.getElementById("g5s2");
+	g5s2.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g5s2.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g5s3 = document.getElementById("g5s3");
+	g5s3.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g5s3.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g5s4 = document.getElementById("g5s4");
+	g5s4.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g5s4.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g5s5 = document.getElementById("g5s5");
+	g5s5.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g5s5.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g5s6 = document.getElementById("g5s6");
+	g5s6.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g5s6.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	//Square Row 6 Event Listeners:
+
+	var g6s1 = document.getElementById("g6s1");
+	g6s1.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g6s1.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g6s2 = document.getElementById("g6s2");
+	g6s2.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g6s2.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g6s3 = document.getElementById("g6s3");
+	g6s3.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g6s3.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g6s4 = document.getElementById("g6s4");
+	g6s4.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g6s4.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g6s5 = document.getElementById("g6s5");
+	g6s5.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g6s5.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+	var g6s6 = document.getElementById("g6s6");
+	g6s6.addEventListener("click", function(){placeDefenderShip(this.id)}, false);
+	g6s6.addEventListener("keyup", function(){changeFocus(this.id)}, false);
+
+//Game Begins, Run Setup
+
+	var stateOfGame = "start"
+
+	var hiddenShips = [];
+
+	var shipsDestroyed = 0;
+
+	var body = document.getElementsByTagName('body')[0];
+	
+	body.style.backgroundImage = 'url(radar.webp)';
+
+	checkStateOfGame();
+
+//Set Arrow and Space Key Constants
+	const VK_LEFT       = 37;
+	const VK_UP         = 38;
+	const VK_RIGHT      = 39;
+	const VK_DOWN       = 40;
+	const SPACE			= 32;
+
+
+function checkStateOfGame(){
+	if (stateOfGame == "start") {
+		defenderMessage();
+		stateOfGame = "placing";
+	}else if (stateOfGame == "attacking") {
+		attackerMessage();
+	}else if(stateOfGame == "Game Over"){
+		gameOver();
+	};
+}
+
+//All Defender Code
+
+		//Hide Attacker Message, Display Defender Message
+		function defenderMessage(){
+
+			document.getElementById("hideships").style.visibility="visible";
+			document.getElementById("hideships_2").style.visibility="visible";
+			document.getElementById("attack").style.visibility="hidden";
+			document.getElementById("attack_2").style.visibility="hidden";
+
+			alert("Player 1 (Defender): Place your ships!");
+
+		}
+
+
+		//Defender clicks to place ships, after 5 ships are placed, switch to attacker mode
+		function placeDefenderShip(clicked_id){
+			if (stateOfGame == "placing" && hiddenShips.length < 5 && !(hiddenShips.includes(clicked_id))){
+				hiddenShips.push(clicked_id);
+				if(hiddenShips.length == 5){
+					stateOfGame = "attacking";
+					checkStateOfGame();
+				}
+			}
+			console.log(hiddenShips);
+		}
+
+
+
+
+//All Attacker Code
+
+	//Hide Defender Message, Display Attacker Message
+		function attackerMessage(){
+
+			document.getElementById("hideships").style.visibility="hidden";
+			document.getElementById("hideships_2").style.visibility="hidden";
+
+			document.getElementById("attack").style.visibility="visible";
+			document.getElementById("attack_2").style.visibility="visible";
+
+			document.getElementById("g1s1").focus();
+
+			alert("All ships placed. Player 1 (Attacker): Sink Enemy Ships!");
+
+		}
+
+
+	//Enable functionality to get all siblings of a given div
+		function getSiblings(el, filter) {
+			    var siblings = [];
+			    el = el.parentNode.firstChild;
+			    do { 
+			    	if (!filter || filter(el)) siblings.push(el); 
+			    } while (el = el.nextSibling);
+			    console.log(siblings);
+			    return siblings;
+			}
+
+	//Enable arrow keys to move focus
+		function changeFocus(selected){
+
+			window.onkeyup = function(e) {
+	   			var key = e.keyCode ? e.keyCode : e.which;
+
+			   if ((stateOfGame != "placing") && (key == VK_LEFT)) {
+			   		document.getElementById(selected).previousElementSibling.focus();
+				}
+				else if ((stateOfGame != "placing" ) && (key == VK_RIGHT)) {
+			    	document.getElementById(selected).nextElementSibling.focus();
+				   }
+				else if((key == SPACE) && (stateOfGame == "attacking")){
+
+					if(hiddenShips.includes(selected)){
+						shipDestroyed(selected);
+					}else{
+						miss(selected);
+					}
+				}
+			}
+		}
+
+	//Increment Score and change visuals on hit and miss events
+		function shipDestroyed(target){
+			console.log("HIT!");
+			shipsDestroyed++;
+			console.log("shipsDestroyed " + shipsDestroyed);
+			document.getElementById(target).style.background="red";
+			document.getElementById(target).innerHTML="HIT!";
+
+			if(shipsDestroyed == 5){
+				stateOfGame = "Game Over"
+				checkStateOfGame();
+			}
+		}
+		function miss(target){
+			document.getElementById(target).style.background="#0720D8";
+			document.getElementById(target).innerHTML="Miss..."
+		}
+
+// All Game Over Code
+
+	function gameOver(){
+		console.log("Game over!")
+		body.style.backgroundImage = 'url(missionaccomplished.gif)';
+	}
+
+
+//Uncomment if you need to torture your strings
+
+// String.prototype.replaceAt=function(index, replacement) {
+//     return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+// }
